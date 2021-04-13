6b35b0ed9fcca612d5021b6bafd13d4b5247038f
LVelasco <LVelasco@archgroup.com>
2021-04-13 17:43:09 -0400

    change name

--- use2-delphix-rg-t/data.tf
+++ use2-delphix-rg-t/data.tf
@@ -3,15 +3,15 @@ data "azurerm_key_vault_secret" "default" {
   name            = "defaultswhkey"
   key_vault_id    = var.keyvault
 }
-data "azurerm_key_vault_secret" "default" {
+data "azurerm_key_vault_secret" "clientID" {
   name            = "clientID"
   key_vault_id    = var.keyvault
 }
-data "azurerm_key_vault_secret" "default" {
+data "azurerm_key_vault_secret" "clientSecret" {
   name            = "clientSecret"
   key_vault_id    = var.keyvault
 }
-data "azurerm_key_vault_secret" "default" {
+data "azurerm_key_vault_secret" "tenantID" {
   name            = "tenantID"
   key_vault_id    = var.keyvault
 }
