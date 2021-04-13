179b221ca2c570d3cc572db6843f47bd28205a02
LVelasco <LVelasco@archgroup.com>
2021-04-13 11:55:05 -0400

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = "~> 0.12.0"
   required_providers {
-    azurerm = "~> 2.11.0"
+    azurerm = "~> 2.46.0"
   }
   backend "azurerm" {
     resource_group_name   = "weu-storage-rg-t"
     storage_account_name  = "weuterraformstate"
     container_name        = "weu"
-    key                   = "weuvnett.tfstate"
+    key                   = "weuvnet2t.tfstate"
   }
 }
 
@@ -16,22 +16,6 @@ provider "azurerm" {
 }
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
+  name     = "weu-vnet2-rg-t"
   location = "westeurope"
-}
-
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "westeurope"
-  resource_group_name = azurerm_resource_group.tamops.name
-}
-
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
 }
