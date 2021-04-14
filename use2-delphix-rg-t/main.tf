6a9209ddfa1b03fc7edcc5a8f41e49a36e578556
LVelasco <LVelasco@archgroup.com>
2021-04-14 07:51:03 -0400

    removed disk

--- use2-delphix-rg-t/main.tf
+++ use2-delphix-rg-t/main.tf
@@ -43,3 +43,41 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address            = each.value
   }
 }
+/*  Create VM */
+resource "azurerm_virtual_machine" "vm" {
+  count                 = vmCount
+  name                  = "${var.prefix}-vm"
+  location              = var.location
+  tags                  = var.tags
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+  admin_ssh_key {
+    username = var.username
+    public_key = data.azurerm_key_vault_secret.default.value
+  }
+  storage_image_reference {
+    publisher = "delphix"
+    offer     = "delphix_dynamic_data_platform"
+    sku       = "delphix_dynamic_data_platform_5-3"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.prefix}-vm-os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+  }
+  boot_diagnostics {
+        storage_account_uri = var.storageaccount
+  }
+  network_interface_ids {
+    azurerm_network_interface.nic.id
+  }
+  azurerm_availability_set {
+    azurerm_availability_set.aset.id
+  }
+}
