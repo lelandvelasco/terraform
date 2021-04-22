9070a78e094a0fc6129349fb2d6ca3b2e5b24fed
Leland <lvvelasco19@outlook.ph>
2021-04-22 14:22:47 -0400

    Update main.tf

--- environment/weu-security-rg-t/main.tf
+++ environment/weu-security-rg-t/main.tf
@@ -1,5 +1,5 @@
 module "keyvault" {
-  source = "./modules/keyvault"
+  source = "https://github.com/lelandvelasco/terraform/tree/master/modules/keyvault"
   backend-tfstate {
     resource_group_name   = "weu-storage-rg-t"
     storage_account_name  = "weuterraformstate"
